762841426a88a42f0ee4d9177be03d0e5d9ef67c
testerpivka2 <uleonidd@gmail.com>
2026-05-13 01:19:14 +0700

    хп бар + потенциально отдельный урон для каждого врага

--- Assets/BarScript.cs
+++ Assets/BarScript.cs
@@ -1,25 +1,47 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BarScript: MonoBehaviour
+public class BarScript : MonoBehaviour
 {
-    private float HP=100f;
-    public Image bar;
-    public void Start()
-    {
+    private float HP = 100f;
+    private Image bar;
 
+    void Start()
+    {
+        bar = GameObject.Find("HPBar").GetComponent<Image>();
     }
 
     void Update()
     {
-        
-    }
-    private void CheckContact(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
+        if (HP < 100)
         {
-            HP -= 5;
-            bar.fillAmount = HP / 100f;
+            HP += 0.01f;
+        }
+
+
+        bar.fillAmount = HP / 100f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && HP > 0)
+        {
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                float damage = enemy.damage; 
+
+                if (HP < damage)
+                {
+                    HP = 0;
+                }
+                else
+                {
+                    HP -= damage;
+                }
+
+            }
         }
     }
 }
